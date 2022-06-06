c18923abb9ae2ac1ea0daf23333745df3899df39
Balazs Marton <bmarton87@gmail.com>
2022-06-07 01:19:22 +0200

    feat: enable cors
    unfortunately, total parity with Node/Flask backend implementations
    was unattainable due to Django being adamant on the trailing slash

--- python_django/python_django/settings.py
+++ python_django/python_django/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# TODO: create developer settings and move there, add prod settings when deployment method is finalised
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
