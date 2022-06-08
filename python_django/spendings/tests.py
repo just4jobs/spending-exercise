e082327c58941c723c81f4b9e7fbd752bcb32fce
Balazs Marton <bmarton87@gmail.com>
2022-06-08 20:32:17 +0200

    test: add test for get

--- python_django/spendings/tests.py
+++ python_django/spendings/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from http import client
+from unittest.mock import patch
+from django.test import (
+    Client,
+    TestCase,
+)
 
-# Create your tests here.
+class TestSpending(TestCase):
+
+    def setUp(self) -> None:
+        self.client = Client()
+        super().setUp()
+
+    @patch('spendings.models.Spending.objects.all')
+    def test_get_spending(self, mock_spendings):
+        mock_spendings.return_value = [{
+            "description": "Mango",
+            "amount": 1200,
+            "spent_at": "2022-02-23T14:47:20.381Z",
+            "currency": "USD"
+        }]
+        response = self.client.get('/spendings/')
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(b'[{"description":"Mango","amount":1200,"spent_at":"2022-02-23T14:47:20.381Z","currency":"USD"}]', response.content)
