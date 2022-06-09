a03e3b9174af6c43ee4f8486d03f6ccf6fddb5e0
Balazs Marton <bmarton87@gmail.com>
2022-06-09 02:20:55 +0200

    test: add post test

--- python_django/spendings/tests.py
+++ python_django/spendings/tests.py
@@ -5,6 +5,7 @@ from django.test import (
     TestCase,
 )
 
+
 class TestSpending(TestCase):
 
     def setUp(self) -> None:
@@ -22,3 +23,15 @@ class TestSpending(TestCase):
         response = self.client.get('/spendings/')
         self.assertEqual(200, response.status_code)
         self.assertEqual(b'[{"description":"Mango","amount":1200,"spent_at":"2022-02-23T14:47:20.381Z","currency":"USD"}]', response.content)
+
+    @patch('spendings.models.Spending.objects.create')
+    def test_post_spending(self, mock_spending_create):
+        new_spending = {
+            "description": "Mango",
+            "amount": 1200,
+            "spent_at": "2022-02-23T14:47:20.381Z",
+            "currency": "USD"
+        }
+        response = self.client.post('/spendings/', new_spending, content_type='application/json')
+        self.assertEqual(200, response.status_code)
+        mock_spending_create.assert_called_with(description="Mango", amount=1200, currency="USD")
